5766f28830036333c8f863ca7e78eb48058f32d2
lyth hammad <lythhammad69@gmail.com>
2023-04-24 16:00:25 -0500

    service admin

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import AutomobileVO, Technician, Appointment
+
+
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Technician)
+class TechnicianAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
