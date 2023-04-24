054008e0eee2fef1923fb5d6a42f1b6c2f6f3fdf
Mo <kanawimou@gmail.com>
2023-04-24 14:31:01 -0700

    added 3/4 models to views and will continue later

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import AutoMobileVO, SalesPerson, Sale, Customer
+import json
+from common.json import ModelEncoder
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+class AutoMobileVOEncoder(ModelEncoder):
+    model = AutoMobileVO
+    properties = [
+        "vin"
+    ]
+
+class SalesPersonEncoder(ModelEncoder):
+    model = SalesPerson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+    ]
