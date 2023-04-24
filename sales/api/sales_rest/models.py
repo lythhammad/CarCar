17cc0cfd44a2de51ab8c095b01c44b129f271ede
Mo <kanawimou@gmail.com>
2023-04-24 13:32:03 -0700

    created sales models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class AutoMobileVO(models.Model):
+    vin = models.CharField(max_length=100, unique=True)
+
+class SalesPerson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutoMobileVO,
+        related_name= "Sale",
+        on_delete=models.CASCADE,
+    )
+    salesperson = models.ForeignKey(
+        SalesPerson,
+        related_name= "Sale",
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name= "Sale",
+        on_delete=models.CASCADE,
+    )
+    price = models.PositiveIntegerField
