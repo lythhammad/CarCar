9374c3096ff447d0d565ceaa153c32b28257b181
lyth hammad <lythhammad69@gmail.com>
2023-04-25 18:52:59 -0500

    fixing fixed views and added poll

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -5,9 +5,6 @@ import time
 import json
 import requests
 
-
-from service.api.service_rest.models import Technician, Appointment
-
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
@@ -15,20 +12,31 @@ django.setup()
 # Import models from service_rest, here.
 # from service_rest.models import Something
 
+from service_rest.models import AutomobileVO
+
+
+def get_vins():
+    response = requests.get("http://localhost:8100/api/manufacturers/")
+    content = json.loads(response.content)
+    for auto in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            import_href=auto["href"],
+            defaults={
+                "vin": auto["vin"],
+            },
+        )
+
 
 def poll(repeat=True):
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
-
+            get_vins
         except Exception as e:
             print(e, file=sys.stderr)
 
         if (not repeat):
             break
-
         time.sleep(60)
 
 
