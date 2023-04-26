e1fee500a3b130d3a2a76a8c21abac41d8ffde80
Mo <kanawimou@gmail.com>
2023-04-25 20:43:03 -0700

    working the app.js and adding routes to sales related files

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import CustomerForm from './CustomerForm';
+import SaleList from './SaleList';
+import SaleForm from './SaleForm';
+import SalesPerson from './SalesPerson';
+import SalesPersonHistory from './SalesPersonHistory';
 
 function App() {
   return (
@@ -9,6 +14,15 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="Sales">
+            <Route path="" element={<SaleList />} />
+            <Route path="new" element={<SaleForm />} />
+            <Route path="customers/new" element={<CustomerForm />} />
+            <Route path="salesperson">
+              <Route path="new" element={<SalesPerson />} />
+              <Route path="history" element={<SalesPersonHistory />} />
+            </Route>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
@@ -16,3 +30,5 @@ function App() {
 }
 
 export default App;
+
+
