fb1bb80ab26a04cd539950e5c58fd86ee3a75f6d
lyth hammad <lythhammad69@gmail.com>
2023-04-27 17:37:44 -0500

    fixing manufactur and TechniciansForm

--- ghi/app/src/ManufacturerForm.js
+++ ghi/app/src/ManufacturerForm.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
 
 function CreateManufacturer() {
-
-const handleSubmit = async (e) => {
-    e.preventDefault();
+const [name, setName] = useState('');
+const handleSubmit = async (event) => {
+    event.preventDefault();
 
     const data = {};
-    data.name = name
+    data.name = name;
 
-    const url = "http://localhost:8100/api/manufacturers/";
+    const manufacturersurl = "http://localhost:8100/api/manufacturers/";
     const fetchConfig = {
         method: "POST",
-        haders: { "Content-Type": "application/json" },
-        body: JSON.stringify({ data }),
+        body: JSON.stringify( data ),
+        headers: { "Content-Type": "application/json",
+        },
     };
-    const response = await fetch(url, fetchConfig);
+    const response = await fetch(manufacturersurl, fetchConfig);
 
     if (response.ok) {
-        const newManufacturer = await response.json()
         setName("");
-        console.log(newManufacturer)
     }
 };
 
-const [name, setName] = useState('');
-const handleNameChange = (e) => {
-    const value = e.target.value;
+const handleNameChange = (event) => {
+    const value = event.target.value;
     setName(value);
-};
-// useEffect(() => {
-//     setName('');
-// },[]);
+}
 
 return (
     <>
@@ -38,9 +34,9 @@ return (
         <div className="offset-3 col-6">
             <div className="shadow p-4 mt-4">
                 <h2>Creat A Manufacturers</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} id="create-location-form">
                     <div className="form-floating mb-3">
-                        <input onChange={handleNameChange} value={name} placeholder="Manufacturers Name" type="text" name="name" className="form-control" />
+                        <input onChange={handleNameChange} value={name} placeholder="Manufacturers Name" type="text" name="name" id="name" className="form-control" />
                         <label htmlFor="name">Manufacturers Name</label>
                     </div>
                     <button className="btn btn-primary">Create</button>
@@ -53,32 +49,6 @@ return (
 }
 
 export default CreateManufacturer;
-
-
-// import React, { useState } from "react";
-
-// function CreateManufacturer() {
-// const [name, setName] = useState("");
-
-// const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     const data = {name:name};
-
-//     const url = "http://localhost:8100/api/manufacturers";
-//     const fetchConfig = {
-//         method: "POST",
-//         haders: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ data }),
-//     };
-//     const response = await fetch(url, fetchConfig);
-
-//     if (response.ok) {
-//         const newManufacturer = await response.json()
-//         console.log(newManufacturer)
-//         setName("");
-//     }
-// };
 
 
 // return (
@@ -102,3 +72,32 @@ export default CreateManufacturer;
 // }
 
 // export default CreateManufacturer;
+
+// function CreateManufacturer() {
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+
+//         const data = {};
+//         data.name = name
+
+//         const url = "http://localhost:8100/api/manufacturers/";
+//         const fetchConfig = {
+//             method: "POST",
+//             haders: { "Content-Type": "application/json" },
+//             body: JSON.stringify({ data }),
+//         };
+//         const response = await fetch(url, fetchConfig);
+
+//         if (response.ok) {
+//             const newManufacturer = await response.json()
+//             setName("");
+//             console.log(newManufacturer)
+//         }
+//     };
+
+//     const [name, setName] = useState('');
+//     const handleNameChange = (e) => {
+//         const value = e.target.value;
+//         setName(value);
+//     };
--- ghi/app/src/TechnicianForm.js
+++ ghi/app/src/TechnicianForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function TechniciansForm({ handleAddTechnician }) {
+function TechniciansForm() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [employeeId, setEmployeeId] = useState("");
@@ -10,22 +10,20 @@ function TechniciansForm({ handleAddTechnician }) {
 
         const data = {}
         data.first_name = firstName;
-        data.last_name = lastName
-        data.employee_id = employeeId
+        data.last_name = lastName;
+        data.employee_id = employeeId;
 
-        const TechnicianUrl = "http://localhost:8080/api/technicians";
+        const technicianUrl = "http://localhost:8080/api/technicians";
         const fetchConfig = {
             method: "POST",
-            body: JSON.stringify(data),
-            Headers: {
+            body: JSON.stringify( data ),
+            headers: {
                 'Content-Type': 'application/json',
             }
         };
 
-        const response = await fetch(TechnicianUrl, fetchConfig);
+        const response = await fetch(technicianUrl, fetchConfig);
         if (response.ok) {
-            const success = document.getElementById("success");
-            success.className = "text-center";
             setFirstName("");
             setLastName("");
             setEmployeeId("");
@@ -44,7 +42,7 @@ function TechniciansForm({ handleAddTechnician }) {
 
     const handleEmployeeIDChange = (event) => {
         const value = event.target.value;
-        setFirstName(value);
+        setEmployeeId(value);
     }
 
 
@@ -54,21 +52,19 @@ return (
             <div className="offset-3 col-6">
                 <div className="shadow p-4 mt-4">
                 <h1>Add a new Technician</h1>
-
                 <form onSubmit={handleSubmit} id="creat-location-form">
-
                     <div className="form-floating mb-3">
-                        <input type="text" value={firstName} onChange= {handleFirstNameChange} placeholder="Name" className="form-control" />
-                        <label htmlFor="name">First Name</label>
+                        <input onChange= {handleFirstNameChange} value={firstName} placeholder="firstName" required type='text' name='firstName' id='firstName' className="form-control" />
+                        <label htmlFor="firstName">First Name</label>
                     </div>
 
                     <div className="form-floating mb-3">
-                        <input type="text" value={lastName} onChange={handleLastNameChange} placeholder="Name" className="form-control" />
-                        <label htmlFor="name">Last Name</label>
+                        <input onChange={handleLastNameChange} value={lastName} placeholder="lastName" type='text' name='lastName' id='lastName' className="form-control" />
+                        <label htmlFor="lastName">Last Name</label>
                     </div>
                     <div className="form-floating mb-3">
-                        <input type="number" value={employeeId} onChange={handleEmployeeIDChange} placeholder="Name" className="form-control" />
-                        <label htmlFor="name">Employee Id</label>
+                        <input onChange={handleEmployeeIDChange} value={employeeId} placeholder="employeeId" required type='number' name='employeeId' id='employeeId' className="form-control" />
+                        <label htmlFor="employeeId">Employee Id</label>
                     </div>
                     <button type="submit" className="btn btn-primary">Create a Technician</button>
                 </form>
@@ -81,16 +77,3 @@ return (
 
 
 export default TechniciansForm;
-
-// const response = await fetch('http://localhost:8080/api/technicians',{
-//             method: "POST",
-//             headers: {
-//                 'content-type': "application/json"
-//             },
-//             body: JSON.stringify({
-//                 firstName,
-//                 lastName,
-//                 employeeId,
-//             })
-
-//         });
