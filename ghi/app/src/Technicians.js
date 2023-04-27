9a527674932c1586b09e614b40f45ddd2dddb076
lyth hammad <lythhammad69@gmail.com>
2023-04-27 01:33:10 -0500

    fixing technicianslist2

--- ghi/app/src/Technicians.js
+++ ghi/app/src/Technicians.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect} from "react";
-import TechniciansForm from "./Technicians";
-
+import React, { useState, useEffect } from "react";
 
 function TechniciansList() {
-    const [technicians, setTechnicians] = useState([]);
+const [technicians, setTechnicians] = useState([]);
 
-    const fetchTechnicians = async () => {
-        const response = await fetch("http://localhost:8080/api/technicians");
-        if (response.ok) {
-            const data = await response.json();
-            setTechnicians(data);
-        }
-    };
+const getData = async () => {
+    const response = await fetch("http://localhost:8080/api/technicians");
+    if (response.ok) {
+    const data = await response.json();
+    setTechnicians(data.technicians);
+    }
+};
 
-    useEffect(() => {
-        fetchTechnicians();
-    }, []);
+useEffect(() => {
+    getData();
+}, []);
 
-
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>Employee ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{technicians.employee_id}</td>
-                    <td>{technicians.first_name}</td>
-                    <td>{technicians.last_name}</td>
-                </tr>
-                </tbody>
-        </table>
-    );
+return (
+    <>
+    <h1>Technicians</h1>
+    <table className="table table-striped">
+        <thead>
+        <tr>
+            <th>Employee ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        {technicians.map((technician) => (
+            <tr key={technician.employee_id}>
+            <td>{technician.employee_id}</td>
+            <td>{technician.first_name}</td>
+            <td>{technician.last_name}</td>
+            </tr>
+        ))}
+        </tbody>
+    </table>
+    </>
+);
 }
+
 export default TechniciansList;
